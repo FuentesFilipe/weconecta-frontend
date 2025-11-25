dd1322eceb97bdf4bbc149b789c2e77ffac6974e
Hartur Kunzler <hartur.kunzler@gmail.com>
2025-11-25 15:05:53 -0300

    atualizacoes para apresentacao

--- next.config.ts
+++ next.config.ts
@@ -3,17 +3,27 @@ import type { NextConfig } from 'next';
 const nextConfig: NextConfig = {
     // Static export (html)
     output: 'export',
-
+    devIndicators: {
+        buildActivity: false,
+    },
     // envs
     env: {
         API_BASE_URL: process.env.API_BASE_URL,
-        ENVIRONMENT: process.env.ENVIRONMENT
+        ENVIRONMENT: process.env.ENVIRONMENT,
     },
 
     // Required if using `next/image` with static export
     images: {
-        unoptimized: true
-    }
+        unoptimized: true,
+    },
+    typescript: {
+        // ❗ Build will succeed even if there are TypeScript errors
+        ignoreBuildErrors: true,
+    },
+    eslint: {
+        // ❗ Build will succeed even if there are ESLint errors
+        ignoreDuringBuilds: true,
+    },
 };
 
 export default nextConfig;
